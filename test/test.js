f078c12636bdd963dc7bc3c4327d233d9ceb1af7
wtetsu <tewatana@gmail.com>
2017-06-18 23:31:04 +0900

    Add tests for string util

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-module.exports.testSomething = (test)=>{
-  test.expect(1);
-  test.ok(true, "this assertion should pass");
-  test.done();
-};
+let string = require("../src/string");
 
-module.exports.testSomethingElse = (test)=>{
-  test.ok(false, "this assertion should fail");
+module.exports.testString = (test)=>{
+  test.deepEqual(string.parseString("word"), ['word']);
+  test.deepEqual(string.parseString("WordoneWordtwoWordthree"), ['WordoneWordtwoWordthree', 'Wordone', 'Wordtwo', 'Wordthree']);
+  test.deepEqual(string.parseString("wordone-wordtwo-wordthree"), ['wordone-wordtwo-wordthree', 'wordone', 'wordtwo', 'wordthree']);
+  test.deepEqual(string.parseString("WORDONE_WORDTWO_WORDTHREE"), ['WORDONE_WORDTWO_WORDTHREE', 'WORDONE', 'WORDTWO', 'WORDTHREE']);
+  test.deepEqual(string.transformWord("running"), ["running", "run"]);
   test.done();
 };
