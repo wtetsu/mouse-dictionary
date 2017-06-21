4d143df9782b5ebb2e039371a45a6922cc8b1264
wtetsu <tewatana@gmail.com>
2017-06-21 20:53:54 +0900

    Add a test for LineReader

---
+++ test/test_linereader.js
@@ -0,0 +1,37 @@
+let LineReader = require("../src/linereader");
+
+module.exports.testLineReaderLf = (test)=>{
+  let reader = new LineReader("aaa\nbbb\nccc");
+
+  let lines = [];
+  reader.eachLine((line)=>{
+    lines.push(line);
+    console.log(line);
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve();
+      }, 100);
+    });
+  }, ()=>{
+    test.deepEqual(["aaa", "bbb", "ccc"], lines);
+    test.done();
+  });
+};
+
+module.exports.testLineReaderCrLf = (test)=>{
+  let reader = new LineReader("aaa\r\nbbb\r\nccc");
+
+  let lines = [];
+  reader.eachLine((line)=>{
+    lines.push(line);
+    console.log(line);
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve();
+      }, 100);
+    });
+  }, ()=>{
+    test.deepEqual(["aaa", "bbb", "ccc"], lines);
+    test.done();
+  });
+};
