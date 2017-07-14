804084adc6c448321ba07f8ceee1da60c976d658
wtetsu <tewatana@gmail.com>
2017-07-14 23:05:33 +0900

    Fix test

--- test/test_string.js
+++ test/test_string.js
@@ -2,9 +2,9 @@ let string = require("../src/string");
 
 module.exports.testString = (test)=>{
   test.deepEqual(string.parseString("word"), ['word']);
-  test.deepEqual(string.parseString("WordoneWordtwoWordthree"), ['WordoneWordtwoWordthree', 'Wordone', 'Wordtwo', 'Wordthree']);
+  test.deepEqual(string.parseString("WordoneWordtwoWordthree"), ['WordoneWordtwoWordthree', 'Wordone', 'wordone', 'Wordtwo', 'wordtwo', 'Wordthree', 'wordthree']);
   test.deepEqual(string.parseString("wordone-wordtwo-wordthree"), ['wordone-wordtwo-wordthree', 'wordone', 'wordtwo', 'wordthree']);
-  test.deepEqual(string.parseString("WORDONE_WORDTWO_WORDTHREE"), ['WORDONE_WORDTWO_WORDTHREE', 'WORDONE', 'WORDTWO', 'WORDTHREE']);
+  test.deepEqual(string.parseString("WORDONE_WORDTWO_WORDTHREE"), ['WORDONE_WORDTWO_WORDTHREE', 'WORDONE', 'wordone', 'WORDTWO', 'wordtwo', 'WORDTHREE', 'wordthree']);
   test.deepEqual(string.transformWord("running"), ["running", "run"]);
   test.done();
 };
