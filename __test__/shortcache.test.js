bca1274a409a069440e22bd636947fc2e94842e0
wtetsu <tewatana@gmail.com>
2018-09-01 22:36:33 +0900

    Add ShortCache

---
+++ __test__/shortcache.test.js
@@ -0,0 +1,50 @@
+import ShortCache from "../src/shortcache";
+
+test("", () => {
+  const cache = new ShortCache(3);
+
+  expect(cache.get("key00")).toEqual(null);
+
+  cache.put("key00", "val00");
+  cache.put("key01", "val01");
+  cache.put("key02", "val02");
+
+  expect(cache.get("key00")).toEqual("val00");
+  expect(cache.get("key01")).toEqual("val01");
+  expect(cache.get("key02")).toEqual("val02");
+
+  cache.put("key03", "val03");
+
+  expect(cache.get("key00")).toEqual(null);
+  expect(cache.get("key01")).toEqual("val01");
+  expect(cache.get("key02")).toEqual("val02");
+  expect(cache.get("key03")).toEqual("val03");
+
+  cache.put("key01", "val01a");
+  cache.put("key02", "val02a");
+
+  expect(cache.get("key00")).toEqual(null);
+  expect(cache.get("key01")).toEqual("val01");
+  expect(cache.get("key02")).toEqual("val02");
+  expect(cache.get("key03")).toEqual("val03");
+
+  cache.put("key03", "val03b");
+  cache.put("key04", "val04b");
+
+  expect(cache.get("key00")).toEqual(null);
+  expect(cache.get("key01")).toEqual(null);
+  expect(cache.get("key02")).toEqual("val02");
+  expect(cache.get("key03")).toEqual("val03");
+  expect(cache.get("key04")).toEqual("val04b");
+
+  cache.put("key05", "val05c");
+  cache.put("key06", "val06c");
+
+  expect(cache.get("key00")).toEqual(null);
+  expect(cache.get("key01")).toEqual(null);
+  expect(cache.get("key02")).toEqual(null);
+  expect(cache.get("key03")).toEqual(null);
+  expect(cache.get("key04")).toEqual("val04b");
+  expect(cache.get("key05")).toEqual("val05c");
+  expect(cache.get("key06")).toEqual("val06c");
+});
---
+++ src/shortcache.js
@@ -0,0 +1,47 @@
+class ShortCache {
+  constructor(size) {
+    this.size = size;
+
+    this.list = new Array(size);
+    this.dict = {};
+    this.index = 0;
+  }
+
+  put(key, value) {
+    if (this.get(key)) {
+      return;
+    }
+
+    let currentData = this.list[this.index];
+    if (currentData) {
+      const oldKey = currentData.key;
+      delete this.dict[oldKey];
+    } else {
+      currentData = {};
+      this.list[this.index] = currentData;
+    }
+    currentData.key = key;
+    currentData.value = value;
+
+    this.dict[key] = this.index;
+
+    this.index = (this.index + 1) % this.size;
+
+    console.warn(this.dict);
+  }
+
+  get(key) {
+    if (!key) {
+      return null;
+    }
+
+    const index = this.dict[key];
+    if (index === undefined) {
+      return null;
+    }
+
+    return this.list[index].value;
+  }
+}
+
+export default ShortCache;
