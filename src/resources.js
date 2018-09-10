7b5acef2b9fb3bc6f9a6ce8c5d61ada7a9d1873e
wtetsu <tewatana@gmail.com>
2018-09-10 20:02:19 +0900

    Add a resource file for the main feature

---
+++ src/resources.js
@@ -0,0 +1,10 @@
+// This resource file is separated from the file of the options UI screen,
+// in order to make the main feature lighter and faster.
+
+const ja = {
+  doesntSupportFrame: "Mouse Dictionaryは、フレームのあるページで使用することはできません。"
+};
+
+export default key => {
+  return ja[key] || null;
+};
