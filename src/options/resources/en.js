0295a58652bd2c88e3b1039444385ae696ef7730
wtetsu <tewatana@gmail.com>
2018-11-03 21:46:16 +0900

    Add the first version of English text

---
+++ src/options/resources/en.js
@@ -0,0 +1,77 @@
+/**
+ * Mouse Dictionary (https://github.com/wtetsu/mouse-dictionary/)
+ * Copyright 2018-present wtetsu
+ * Licensed under MIT
+ */
+
+export default {
+  dictDataEncoding: "Character set of dictionary data：",
+  dictDataFormat: "Format of dictionary data：",
+  readDictData: "Loading dictionary data：",
+
+  selectDictFile: "Select dictionary data.",
+  progressRegister: "%d words have registered(%s)",
+  finishRegister: "Loading has finished(%d words)",
+  clearAllDictData: "Are you sure to clear all the dictionary data?",
+  finishedClear: "All the dictionary data has removed",
+  dictDataUsage: "%s KB used",
+  confirmLoadInitialDict: "There is no dictionary data yet.\nDo you want to register free dictionary data?",
+
+  formatEijiroText: "EIJIRO text data",
+  formatTsv: "TSV(tab-separated text data)",
+  formatPdicOneLine: "PDIC",
+  formatJson: "JSON",
+
+  positionLeft: "Left",
+  positionRight: "Right",
+  positionKeep: "Restore the last position and size",
+
+  scrollOff: "Available",
+  scrollOn: "Not available",
+
+  saveSettings: "Save settings",
+  defaultSettings: "Reset to default",
+  finishSaving: "Saved.",
+
+  openBasicSettings: "Open settings",
+  closeBasicSettings: "Close settings",
+  openAdvancedSettings: "Open advanced settings",
+  closeAdvancedSettings: "Open advanced settings",
+
+  basicSettings: "Basic settings",
+  trialText: "Text for trial",
+  abbreviateShortWordDesc: "Truncate short words descriptions",
+  abbreviateShortWordDesc0: "Truncate",
+  abbreviateShortWordDesc1: "-letter or less word descriptions into",
+  abbreviateShortWordDesc2: "characters",
+
+  initialSize: "Initial size",
+  width: "Width",
+  height: "Height",
+  initialPosition: "Initial position",
+  scrollBar: "Scroll bar",
+  colorAndFont: "Font size and color",
+  backgroundColor: "Background color",
+  headFontSize: "Header font size",
+  headFontColor: "Header font color",
+  descFontSize: "Description font size",
+  descFontcolor: "Description font color",
+
+  replaceRules: "Rules to replace text ",
+  replaceRule1: " ",
+  replaceRule2: " ",
+  advancedSettings: "Advanced settings",
+  htmlTemplate: "HTML templates",
+  htmlTemplateWindow: "Mouse Dictionary window frame",
+  htmlTemplateTitleBar: "Title bar",
+  htmlTemplateDesc: "Description frame",
+  htmlTemplateDescText: "Description",
+  styles: "Style of each state",
+  stylesActive: "Style(active)",
+  stylesMoving: "Style(moving)",
+  stylesInactive: "Style(not active)",
+  lookupWithCapitalized: "Lookup capitalized headers too",
+  showTitlebar: "Show title bar",
+  parseWordsLimit: "Number of words to parse at once",
+  add: "Add"
+};
