3abcc4172407b9b53fbbddccfdb30cf8c4c063ea
wtetsu <tewatana@gmail.com>
2018-11-08 21:35:02 +0900

    Fix tests

--- __test__/resource.test.js
+++ __test__/resource.test.js
@@ -1,6 +1,11 @@
 import res from "../src/options/resources";
 
 test("", () => {
-  expect(res("finishRegister", 999)).toEqual("登録完了(999語)");
-  expect(res("progressRegister", 999, "hello")).toEqual("999語登録(hello)");
+  res.setLang("ja");
+  expect(res.get("finishRegister", 999)).toEqual("登録完了(999語)");
+  expect(res.get("progressRegister", 999, "hello")).toEqual("999語登録(hello)");
+
+  res.setLang("en");
+  expect(res.get("finishRegister", 999)).toEqual("Loading has finished(999 words)");
+  expect(res.get("progressRegister", 999, "hello")).toEqual("999 words have registered(hello)");
 });
