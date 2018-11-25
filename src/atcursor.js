b9b76849bb46199757e7d8a916ba3edd82fe7825
wtetsu <tewatana@gmail.com>
2018-11-25 13:30:56 +0900

    Improve traversing nodes

--- src/atcursor.js
+++ src/atcursor.js
@@ -8,19 +8,69 @@ import consts from "./consts";
 
 export default (element, clientX, clientY, maxWords = 5) => {
   let textOnCursor = null;
-
   try {
-    const range = getCaretNodeAndOffsetFromPoint(element.ownerDocument, clientX, clientY);
-    if (range) {
-      const { node, offset } = range;
-      if (node.nodeType === Node.TEXT_NODE) {
-        textOnCursor = getTextFromRange(node.data, offset, maxWords);
-      }
-    }
+    textOnCursor = fetchTextOnCursor(element, clientX, clientY, maxWords);
   } catch (err) {
     textOnCursor = null;
   }
   return removeQuotes(textOnCursor);
+};
+
+const fetchTextOnCursor = (element, clientX, clientY, maxWords) => {
+  const range = getCaretNodeAndOffsetFromPoint(element.ownerDocument, clientX, clientY);
+  if (!range) {
+    return null;
+  }
+  const { node, offset } = range;
+  if (node.nodeType !== Node.TEXT_NODE) {
+    return null;
+  }
+  return fetchTextFromTextNode(node, offset, maxWords);
+};
+
+const fetchTextFromTextNode = (textNode, offset, maxWords) => {
+  let textOnCursor;
+  const { text, end } = getTextFromRange(textNode.data, offset, maxWords);
+  if (!end) {
+    textOnCursor = text;
+  } else {
+    const siblingsText = fetchSiblingsText(textNode);
+    if (siblingsText) {
+      const concatenatedText = text + " " + siblingsText;
+      const endIndex = searchEndIndex(concatenatedText, 0, maxWords);
+      textOnCursor = concatenatedText.substring(0, endIndex);
+    } else {
+      textOnCursor = text;
+    }
+  }
+  return textOnCursor;
+};
+
+const fetchSiblingsText = endNode => {
+  const node = endNode.parentNode;
+  const children = node.parentElement && node.parentElement.children;
+  if (!children || children.length === 0) {
+    return "";
+  }
+  let selfIndex;
+  for (selfIndex = 0; selfIndex < children.length; selfIndex++) {
+    const child = children[selfIndex];
+    if (node === child) {
+      break;
+    }
+  }
+  let text = "";
+  for (let i = selfIndex + 1; i < children.length; i++) {
+    if (text.length >= 30) {
+      break;
+    }
+    const child = children[i];
+    const t = child.textContent && child.textContent.trim();
+    if (t) {
+      text += " " + t;
+    }
+  }
+  return text;
 };
 
 const removeQuotes = text => {
@@ -106,18 +156,18 @@ const searchEndIndex = (text, index, maxWords) => {
 
 const getTextFromRange = (text, offset, maxWords) => {
   if (!text) {
-    return null;
+    return {};
   }
   const code = text.charCodeAt(offset);
   const isEnglishLikeCharacter = 0x20 <= code && code <= 0x7e;
   if (!isEnglishLikeCharacter) {
-    return null;
+    return {};
   }
-
   const startIndex = searchStartIndex(text, offset);
   const endIndex = searchEndIndex(text, offset, maxWords);
-
-  return text.substring(startIndex, endIndex);
+  const resultText = text.substring(startIndex, endIndex);
+  const end = endIndex >= text.length;
+  return { text: resultText, end };
 };
 
 let getCaretNodeAndOffsetFromPoint = (ownerDocument, pointX, pointY) => {
