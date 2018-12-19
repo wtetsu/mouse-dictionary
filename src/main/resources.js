ab2b9fcd6c531c1ccde314ca39ba1a8f64143d80
wtetsu <tewatana@gmail.com>
2018-12-19 19:48:36 +0900

    Support English error messages

--- src/main/resources.js
+++ src/main/resources.js
@@ -7,10 +7,34 @@
 // This resource file is separated from the file of the options UI screen,
 // in order to make the main feature lighter and faster.
 
-const ja = {
-  doesntSupportFrame: "Mouse Dictionaryは、フレームのあるページで使用することはできません。"
+const resources = {
+  ja: {
+    doesntSupportFrame: "Mouse Dictionaryは、フレームのあるページで使用することはできません。"
+  },
+  en: {
+    doesntSupportFrame: "Mouse Dictionary doesn't support frame pages."
+  }
+};
+
+const decideLanguage = () => {
+  let result = "en";
+  const languages = navigator.languages;
+  if (!languages) {
+    return result;
+  }
+  const validLanguages = Object.keys(resources);
+  for (let i = 0; i < languages.length; i++) {
+    const lang = languages[i].toLowerCase().split("-")[0];
+    if (validLanguages.includes(lang)) {
+      result = lang;
+      break;
+    }
+  }
+  return result;
 };
 
 export default key => {
-  return ja[key] || null;
+  const lang = decideLanguage();
+  const res = resources[lang];
+  return res[key] || null;
 };
