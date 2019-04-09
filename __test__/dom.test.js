14aeb3212abbd20864558db1c5634614e22a7b06
wtetsu <tewatana@gmail.com>
2019-04-09 20:04:26 +0900

    Add dom tests

---
+++ __test__/dom.test.js
@@ -0,0 +1,6 @@
+import dom from "../src/lib/dom";
+
+test("", () => {
+  const e = dom.create("<div><span>aaa</span><span>bbb</span><span>ccc</span></div>");
+  expect(e.children.length).toEqual(3);
+});
