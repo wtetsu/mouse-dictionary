9900cefbdcb9016473b9726cf3d7082aaa8521d8
wtetsu <tewatana@gmail.com>
2019-06-26 21:34:17 +0900

    Make storage.js great again
    See also: #16

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -4,101 +4,59 @@
  * Licensed under MIT
  */
 
+const locals = {
+  get: (...args) => chrome.storage.local.get(...args),
+  set: (...args) => chrome.storage.local.set(...args),
+  getBytesInUse: (...args) => chrome.storage.local.getBytesInUse(...args)
+};
+
+const syncs = {
+  get: (...args) => chrome.storage.sync.get(...args),
+  set: (...args) => chrome.storage.sync.set(...args)
+};
+
 // A wrapper of chrome.storage.sync.
 const sync = {
-  async get(keys) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.get(keys, data => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve(data);
-        }
-      });
-    });
-  },
-  async set(items) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.set(items, () => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve();
-        }
-      });
-    });
-  },
+  get: async args => doAsync(syncs.get, args),
+  set: async args => doAsync(syncs.set, args),
   async pickOut(key) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.get(key, data => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve(data[key]);
-        }
-      });
-    });
+    const data = await sync.get(key);
+    return data && data[key];
   }
 };
 
 const local = {
-  async get(keys) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.get(keys, data => {
+  get: async args => doAsync(locals.get, args),
+  set: async args => doAsync(locals.set, args),
+  async pickOut(key) {
+    const data = await local.get(key);
+    return data && data[key];
+  },
+  getBytesInUse: async () => doAsync(locals.getBytesInUse)
+};
+
+const doAsync = async (fn, params) => {
+  if (!fn) {
+    return null;
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      const callBack = data => {
         if (chrome.runtime.lastError) {
           reject(chrome.runtime.lastError);
         } else {
           resolve(data);
         }
-      });
-    });
-  },
-  async set(items) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.set(items, () => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve();
-        }
-      });
-    });
-  },
-  async pickOut(key) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.get(key, data => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve(data[key]);
-        }
-      });
-    });
-  },
-  async clear() {
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.clear(() => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve();
-        }
-      });
-    });
-  },
-  async getBytesInUse() {
-    // note: Firefox doesn't support getBytesInUse(at least 62.0)
-    //return doAsync(chrome.storage.local.getBytesInUse);
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.getBytesInUse(bytesInUse => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve(bytesInUse);
-        }
-      });
-    });
-  }
+      };
+      if (params) {
+        fn(params, callBack);
+      } else {
+        fn(callBack);
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 export default { sync, local };
