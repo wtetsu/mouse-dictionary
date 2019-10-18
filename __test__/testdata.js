c5eee1e2a2fea2a48bceaf6fc1079bf177ad6009
wtetsu <tewatana@gmail.com>
2019-10-18 23:19:28 +0900

    Fix tests

--- __test__/testdata.js
+++ __test__/testdata.js
@@ -1,18 +1,19 @@
 import fs from "fs";
+import json5 from "json5";
 import rule from "../src/main/rule";
 
 const load = () => {
-  rule.registerNoun(readJson("noun.json"));
-  rule.registerPhrase(readJson("phrase.json"));
-  rule.registerPronoun(readJson("pronoun.json"));
-  rule.registerSpelling(readJson("spelling.json"));
-  rule.registerTrailing(readJson("trailing.json"));
-  rule.registerVerb(readJson("verb.json"));
+  rule.registerNoun(readJson("noun.json5"));
+  rule.registerPhrase(readJson("phrase.json5"));
+  rule.registerPronoun(readJson("pronoun.json5"));
+  rule.registerSpelling(readJson("spelling.json5"));
+  rule.registerTrailing(readJson("trailing.json5"));
+  rule.registerVerb(readJson("verb.json5"));
 };
 
 const readJson = fileName => {
   const json = fs.readFileSync(`rule/${fileName}`, "utf8");
-  return JSON.parse(json);
+  return json5.parse(json);
 };
 
 export default { load };
