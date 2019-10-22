02b9df8c57ea2547fb9b565f796aae4947206a75
wtetsu <tewatana@gmail.com>
2019-10-22 12:46:48 +0900

    Improve tests

--- __test__/resource.test.js
+++ __test__/resource.test.js
@@ -4,8 +4,15 @@ test("", () => {
   res.setLang("ja");
   expect(res.get("finishRegister", 999)).toEqual("登録完了(999語)");
   expect(res.get("progressRegister", 999, "hello")).toEqual("999語登録(hello)");
+  expect(res.get("invalidKey")).toEqual("invalidKey");
 
   res.setLang("en");
   expect(res.get("finishRegister", 999)).toEqual("Loading has finished(999 words)");
   expect(res.get("progressRegister", 999, "hello")).toEqual("999 words have registered(hello)");
+  expect(res.get("invalidKey")).toEqual("invalidKey");
+
+  res.setLang("invalid_language");
+  expect(res.get("finishRegister", 999)).toEqual("Loading has finished(999 words)");
+  expect(res.get("progressRegister", 999, "hello")).toEqual("999 words have registered(hello)");
+  expect(res.get("invalidKey")).toEqual("invalidKey");
 });
