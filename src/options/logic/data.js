cc0129b506f141c6943510528520cfcb14f84633
wtetsu <tewatana@gmail.com>
2019-10-26 23:25:43 +0900

    Minor polishing

--- src/options/logic/data.js
+++ src/options/logic/data.js
@@ -9,19 +9,15 @@ const byteArrayMayBeShiftJis = array => {
   let nextShouldSecondByte = false;
   for (let index = 0; index < array.length; index++) {
     const byte = array[index];
-    const isSecondByte = (byte >= 0x40 && byte <= 0x7e) || (byte >= 0x80 && byte <= 0xfc);
     if (!nextShouldSecondByte) {
-      const isFirstByte = (byte >= 0x81 && byte <= 0x9f) || (byte >= 0xe0 && byte <= 0xef);
-      const isSoleChar = (byte >= 0x00 && byte <= 0x1f) || (byte >= 0x20 && byte <= 0x7f) || (byte >= 0xa1 && byte <= 0xdf);
-
-      if (isFirstByte) {
+      if (isShiftJisFirstByte(byte)) {
         nextShouldSecondByte = true;
-      } else if (!isSoleChar) {
+      } else if (!isShiftJisSoleChar(byte)) {
         mayBeShiftJis = false;
         break;
       }
     } else {
-      if (isSecondByte) {
+      if (isShiftJisSecondByte(byte)) {
         nextShouldSecondByte = false;
       } else {
         mayBeShiftJis = false;
@@ -30,6 +26,18 @@ const byteArrayMayBeShiftJis = array => {
     }
   }
   return mayBeShiftJis;
+};
+
+const isShiftJisFirstByte = byte => {
+  return (byte >= 0x81 && byte <= 0x9f) || (byte >= 0xe0 && byte <= 0xef);
+};
+
+const isShiftJisSecondByte = byte => {
+  return (byte >= 0x40 && byte <= 0x7e) || (byte >= 0x80 && byte <= 0xfc);
+};
+
+const isShiftJisSoleChar = byte => {
+  return (byte >= 0x00 && byte <= 0x1f) || (byte >= 0x20 && byte <= 0x7f) || (byte >= 0xa1 && byte <= 0xdf);
 };
 
 export default {
