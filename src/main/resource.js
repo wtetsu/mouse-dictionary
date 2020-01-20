6138dbcdfaac7644f37c6ab76763f1d82d70c0d8
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-01-20 22:56:10 +0900

    Show initial setup guide for Firefox

--- src/main/resource.js
+++ src/main/resource.js
@@ -9,14 +9,23 @@
 
 const resources = {
   ja: {
-    doesntSupportFrame: "Mouse Dictionaryは、フレームのあるページで使用することはできません。",
-    needToPrepareDict: "初めに辞書データをロードしてください(拡張のアイコンを右クリック→「オプション」)"
+    doesntSupportFrame: "Mouse Dictionaryは、フレームのあるページで使用することはできません。"
   },
   en: {
-    doesntSupportFrame: "Mouse Dictionary doesn't support frame pages.",
-    needToPrepareDict: 'Please load dictionary data first. Right click on the extension icon and select "Options"'
+    doesntSupportFrame: "Mouse Dictionary doesn't support frame pages."
   }
 };
+
+if (BROWSER === "FIREFOX") {
+  resources.ja.needToPrepareDict =
+    "初めに辞書データをロードしてください(拡張のアイコンを右クリック→「拡張機能を管理」→「...」をクリック→「オプション」)";
+  resources.en.needToPrepareDict =
+    'Please load dictionary data first. Right click on the extension icon, select "Manage Extension", click "…", and select "Options"';
+} else {
+  resources.ja.needToPrepareDict = "初めに辞書データをロードしてください(拡張のアイコンを右クリック→「オプション」)";
+  resources.en.needToPrepareDict =
+    'Please load dictionary data first. Right click on the extension icon and select "Options"';
+}
 
 const decideLanguage = () => {
   let result = "en";
