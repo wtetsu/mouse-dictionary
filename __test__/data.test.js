f20d9b710bc28fdd4d0b3fc39986b81af0afff41
wtetsu <tewatana@gmail.com>
2020-01-22 20:31:12 +0900

    Add some tests for data.js

--- __test__/data.test.js
+++ __test__/data.test.js
@@ -1,4 +1,5 @@
 import data from "../src/options/logic/data";
+import defaultsettings from "../src/settings/defaultsettings";
 
 test("", () => {
   expect(true).toEqual(data.byteArrayMayBeShiftJis([]));
@@ -15,4 +16,19 @@ test("", () => {
   expect(false).toEqual(data.byteArrayMayBeShiftJis([0x9f, 0x7f]));
   expect(false).toEqual(data.byteArrayMayBeShiftJis([0xe0, 0x7f]));
   expect(false).toEqual(data.byteArrayMayBeShiftJis([0xef, 0xfd]));
+});
+
+test("", () => {
+  const d1 = defaultsettings;
+  const d2 = data.preProcessSettings(d1);
+  const d3 = data.postProcessSettings(d1);
+  expect(false).toEqual(d1 === d2);
+  expect(false).toEqual(d2 === d3);
+
+  expect(0).toEqual(d1.replaceRules.filter(r => r.key).length);
+  expect(d1.replaceRules.length).toEqual(d1.replaceRules.length);
+  expect(d2.replaceRules.length).toEqual(d2.replaceRules.filter(r => r.key).length);
+  expect(0).toEqual(d3.replaceRules.filter(r => r.key).length);
+
+  expect(JSON.stringify(d1)).toEqual(JSON.stringify(d3));
 });
