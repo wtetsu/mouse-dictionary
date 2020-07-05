8f24e8a630fbc463d3479fd4274f31682775538a
wtetsu <tewatana@gmail.com>
2020-07-05 23:40:36 +0900

    Improve wa-eijiro parsing

--- __test__/eijiroparser.test.js
+++ __test__/eijiroparser.test.js
@@ -11,9 +11,17 @@ test("", () => {
   hd = parser.addLine("■when : 【レベル】＊＊＊＊＊＊＊＊＊＊");
   expect(hd).toEqual(null);
 
-  hd = parser.addLine("■tile  {自動} : 《コ》〔＊＊＊＊＊＊〕＊＊＊＊＊＊＊＊");
+  hd = parser.addLine("■アイウエオ {人名} : あいうえお〔火〕");
   expect(hd.head).toEqual("when");
   expect(hd.desc).toEqual("{代名} : ＊＊\n{名} : 〔＊＊＊＊＊＊＊〕＊＊＊＊＊\n【レベル】＊＊＊＊＊＊＊＊＊＊");
+
+  hd = parser.addLine("■がぎぐげご〔動物が〕 {形} : ざじずぜぞ");
+  expect(hd.head).toEqual("アイウエオ");
+  expect(hd.desc).toEqual("{人名} : あいうえお〔火〕");
+
+  hd = parser.addLine("■tile  {自動} : 《コ》〔＊＊＊＊＊＊〕＊＊＊＊＊＊＊＊");
+  expect(hd.head).toEqual("がぎぐげご");
+  expect(hd.desc).toEqual("〔動物が〕 {形} : ざじずぜぞ");
 
   hd = parser.addLine("■tile  {他動-1} : ＊＊＊＊");
   expect(hd).toEqual(null);
--- src/options/logic/eijiroparser.js
+++ src/options/logic/eijiroparser.js
@@ -6,7 +6,7 @@
 
 const HEADWORD_FIRST = "■";
 const DELIMITER1 = " : ";
-const DELIMITER2 = "  {";
+const SPECIAL_DELIMITERS = ["  {", "〔", " {"];
 const NEW_LINE = "\n";
 
 export default class EijiroParser {
@@ -45,16 +45,28 @@ export default class EijiroParser {
     }
 
     const firstHalf = line.substring(1, dindex1);
-    const dindex2 = firstHalf.indexOf(DELIMITER2);
-    const result = {};
-    if (dindex2 >= 1) {
-      result.head = line.substring(1, dindex2 + 1);
-      result.desc = line.substring(dindex2 + 3);
-    } else {
-      result.head = firstHalf;
-      result.desc = line.substring(dindex1 + DELIMITER1.length);
+
+    let result = null;
+    for (let i = 0; i < SPECIAL_DELIMITERS.length; i++) {
+      const delimiter = SPECIAL_DELIMITERS[i];
+
+      const dindex2 = firstHalf.indexOf(delimiter);
+      if (dindex2 >= 1) {
+        result = {
+          head: line.substring(1, dindex2 + 1),
+          desc: line.substring(dindex2 + delimiter.length),
+        };
+        break;
+      }
     }
-    return result;
+    if (result) {
+      return result;
+    }
+
+    return {
+      head: firstHalf,
+      desc: line.substring(dindex1 + DELIMITER1.length),
+    };
   }
 
   flush() {
