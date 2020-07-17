7f53a1215cd247ec5fbba04d01c10b95c369e0b4
wtetsu <tewatana@gmail.com>
2020-07-17 22:21:49 +0900

    Restructure

--- src/options/app.tsx
+++ src/options/app.tsx
@@ -8,7 +8,7 @@ import "@babel/polyfill";
 import swal from "sweetalert";
 import React from "react";
 import { render } from "react-dom";
-import { Main } from "./component/Main";
+import { Main } from "./page/Main";
 import rule from "../main/rule";
 
 import ace from "ace-builds/src-noconflict/ace";
